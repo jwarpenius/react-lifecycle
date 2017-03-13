20cacace580657dd7f486fba135bca9e50e028b4
Jari Warpenius <jari@warpenius.fi>
2017-03-14 01:26:57 +0200

    initial

---
+++ app/index.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ParentComponent = require('ParentComponent');
+ReactDOM.render(
+  <ParentComponent />,
+  document.getElementById('app')
+);
